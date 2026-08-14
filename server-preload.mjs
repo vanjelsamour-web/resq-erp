d28797082c78b384c69fc3deac6346a879a68db9
vanjelsamour-web <vanjelsamour@gmail.com>
2026-08-14 23:19:40 +0300

    Load invoice creation enhancement

--- server-preload.mjs
+++ server-preload.mjs
@@ -1,11 +1,13 @@
 import express from 'express';
 import { installEnhancements } from './server-enhancements.mjs';
+import { installInvoiceCreate } from './invoice-create-enhancements.mjs';
 
 const originalListen = express.application.listen;
 if (!express.application.__resqListenPatched) {
   express.application.__resqListenPatched = true;
   express.application.listen = function (...args) {
     installEnhancements(this);
+    installInvoiceCreate(this);
     return originalListen.apply(this, args);
   };
 }
